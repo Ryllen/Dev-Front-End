b88defd35775d4cce9ce84542ea105306e4fadc0
Ryllen <alvesryllen@gmail.com>
2026-09-10 20:13:16 -0300

    Resolvendo o erro 404

--- js/api.js
+++ js/api.js
@@ -1,40 +1,20 @@
 export async function carregarTarefas() {
-
-    const resposta = await fetch("../dados.json");
-
+    const resposta = await fetch("./dados.json");
 
     if (!resposta.ok) {
-
-        const erro = new Error(
-            `Erro HTTP ${resposta.status}`
-        );
-
+        const erro = new Error(`Erro HTTP ${resposta.status}`);
         erro.name = "HttpError";
-
         erro.status = resposta.status;
-
         throw erro;
     }
-
 
     const dados = await resposta.json();
 
-
-    if (
-        !dados ||
-        typeof dados !== "object" ||
-        !Array.isArray(dados.tarefas)
-    ) {
-
-        const erro = new Error(
-            "Estrutura JSON inválida."
-        );
-
+    if (!dados || typeof dados !== "object" || !Array.isArray(dados.tarefas)) {
+        const erro = new Error("Estrutura JSON inválida.");
         erro.name = "FormatError";
-
         throw erro;
     }
-
 
     return dados.tarefas;
 }
